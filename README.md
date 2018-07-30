99a3b91a703b93c59566fa29da36d82e78bd82ce
IetermagogSA <39064341+IetermagogSA@users.noreply.github.com>
2018-07-30 20:23:58 +0200

    Importing files
    Importing files to the project

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # StringRotations_Challenge
+
+
+https://www.sololearn.com/learn/9885/?ref=app
+(Link works only in SoloLearn App)
+
+String Rotations
+
+Create a function that accepts a string argument and returns an array of strings, which are shifted versions of the argument string.
+
+Sample Input
+"Hello"
+
+Sample Output
+["elloH", "lloHe", "loHel", "oHell", "Hello"]  
+
+As you can see from the example above, each element shifts the first character of the previous version to the end.
+The original string should be included in the output array. The order matters; each element of the output array should be the shifted version of the previous element.
+The output array should have the same length as the input string. 
+The function should return an empty array when a zero-length String is provided as the argument.
---
+++ StringRotation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the word you want to shift around: ");
+            string[] shiftedString = shiftString(Console.ReadLine());
+
+            foreach (string word in shiftedString)
+            {
+                Console.Write(word + " ");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static string[] shiftString(string myString)
+        {
+            Stack<string> shiftedStack = new Stack<string>();
+
+            char[] afterShiftString = myString.ToCharArray();
+
+            for (int i = 0; i <= myString.Length - 1; i++)
+            {
+                shiftedStack.Push(new string(afterShiftString));
+
+                char lastChar = afterShiftString.Last();
+                for (int c = myString.Length - 1; c > 0; c--)
+                {
+                    afterShiftString[c] = afterShiftString[c - 1];
+                }
+                afterShiftString[0] = lastChar;
+            }
+
+            return shiftedStack.ToArray();
+        }
+    }
+}
